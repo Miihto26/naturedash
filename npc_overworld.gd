73e8eb00d936970d7397e673eb4958921eabf73f
stxrrymuffin <jessica.chen270412@gmail.com>
2025-04-21 21:09:31 -0400

    ugh base overworld w/o art in

--- npc_overworld.gd
+++ npc_overworld.gd
@@ -4,3 +4,29 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)) and not Signals.inDialogue:
 			DialogueManager.show_dialogue_balloon(load("res://dialogue/starting.dialogue"), self.name)
+			var nodeName = self.name
+			Signals.npcInteracted[String(nodeName)[4].to_int()-1] = 1
+			get_parent().get_node("Indicator" + String(nodeName)[4]).hide()
+			if String(nodeName)[4].to_int() <=2:
+				incCompassion()
+			else:
+				incConnectivity()
+			decEcoAnxiety()
+
+func incCompassion():
+	Signals.compassion = Signals.compassion + 20
+	while get_parent().get_node("ProgressionBars").get_node("compassion").value != Signals.compassion:
+		await get_tree().create_timer(0.1).timeout
+		get_parent().get_node("ProgressionBars").get_node("compassion").value += 1
+
+func incConnectivity():
+	Signals.connectivity = Signals.connectivity + 20
+	while get_parent().get_node("ProgressionBars").get_node("connectivity").value != Signals.connectivity: 
+		await get_tree().create_timer(0.1).timeout
+		get_parent().get_node("ProgressionBars").get_node("connectivity").value += 1
+
+func decEcoAnxiety():
+	Signals.ecoanxiety = Signals.ecoanxiety - 20
+	while get_parent().get_node("ProgressionBars").get_node("ecoanxiety").value != Signals.ecoanxiety:
+		await get_tree().create_timer(0.1).timeout
+		get_parent().get_node("ProgressionBars").get_node("ecoanxiety").value -= 1
---
+++ town.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var listIcons = [0,1,2,3,4]
+	listIcons.shuffle()
+	var selectedItems = [listIcons[0], listIcons[1]]
+	print(selectedItems)
+	var allNodes = [get_node("Icon1"), get_node("Icon2"),get_node("Icon3"),get_node("Icon4"),get_node("Icon5")]
+	var allIndicators = [get_node("Indicator1"), get_node("Indicator2"),get_node("Indicator3"),get_node("Indicator4"),get_node("Indicator5")]
+		
+		
+	for i in range(len(allNodes)):
+		if i in selectedItems:
+			allNodes[i].show()
+			if Signals.npcInteracted[i] == 1:
+				allIndicators[i].hide()
+			else:
+				allIndicators[i].show()
+		else:
+			allNodes[i].hide()
+			allIndicators[i].hide()
+	#if all selected arent new then reshuffle
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
